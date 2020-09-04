6e08d681fe6e7085c740725082f43693637e1aca
user-oyym <70590277+user-oyym@users.noreply.github.com>
2020-09-04 15:13:45 +0800

    Update GitHubTest.java

--- src/com/ryit/github/GitHubTest.java
+++ src/com/ryit/github/GitHubTest.java
@@ -3,6 +3,7 @@ package com.ryit.github;
 public class GitHubTest {
 	public static void main(String... args) {
 		System.out.println("Hello");
-                System.out.println("resvise");
+        System.out.println("resvise");
+		System.out.println("my 通过 GitHub Desktop 打开 Notepad++ 进行了resvise");
 	}
 }
